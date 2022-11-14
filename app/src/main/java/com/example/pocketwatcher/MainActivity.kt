cdf8c6d2c44fb2d14ff0c7a447fa292609a5e72b
root <ryan.jingwi@gmail.com>
2022-11-14 13:44:09 -0500

    Finished coding bottom navigation

--- app/src/main/java/com/example/pocketwatcher/MainActivity.kt
+++ app/src/main/java/com/example/pocketwatcher/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.pocketwatcher
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    lateinit var BottomNav: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        BottomNav = findViewById(R.id.BottomNav)
+        supportFragmentManager.beginTransaction().replace(R.id.Frame, OverviewFragment()).commit()
+        BottomNav.setOnNavigationItemSelectedListener(BottomNavigationView.OnNavigationItemSelectedListener {
+            lateinit var selectedfragement:Fragment
+            when(it.itemId){
+                R.id.Overview->{
+                    selectedfragement = OverviewFragment()
+                }
+                R.id.Transactions->{
+                    selectedfragement = TransactionsFragment()
+                }
+            }
+            supportFragmentManager.beginTransaction().replace(R.id.Frame, selectedfragement).commit()
+            true
+        })
     }
 }
